766bd8fafed2913a43ed8c3facb7e4a94c4af8aa
ChenTao <chentao_china@outlook.com>
2022-11-10 01:58:31 +0800

    update

--- Sync.py
+++ Sync.py
@@ -5,133 +5,6 @@ from MyGitlab import *
 from MyLDAP import *
 
 
-def ldap_user_to_gitlab(ldap_user_attr: dict[int, Any], dn: str, ldap_provider='ldapmaiin') -> MyUser:
-    user = MyUser(id=-1,
-                  username=ldap_user_attr['uid'],
-                  name=ldap_user_attr['cn'],
-                  email=ldap_user_attr['mail']
-                  )
-    user.ext_ID.uid = dn
-    user.ext_ID.provider = ldap_provider
-    return user
-
-
-def create_group_in_gitlab_by_ldap(ldap_group: SimpleGroup, mygitlab: MyGitlab, new_group_visibility='private') -> int:
-    group_info = GitlabGroup(name=ldap_group.name,
-                             visibility=new_group_visibility
-                             )
-    if ldap_group.description is not None and ldap_group.description == '':
-        group_info.description = ldap_group.description
-    group_id = mygitlab.group_create(group_info)
-    return group_id
-
-
-def check_group_member_in_gitlab(ldap_group: SimpleGroup,
-                                 mygitlab: MyGitlab,
-                                 check_attr='extern_uid',
-                                 new_group_visibility='private'
-                                 ) -> tuple[bool, MyGroup, list[str]]:
-    absense_items = ldap_group.members
-    gitlab_group_names = mygitlab.mygroup_all.names
-    create = ldap_group.name not in gitlab_group_names
-    if create:
-        group_id = create_group_in_gitlab_by_ldap(ldap_group=ldap_group,
-                                                  mygitlab=mygitlab,
-                                                  new_group_visibility=new_group_visibility
-                                                  )
-        gitlab_group = MyGroup(id=group_id, name=ldap_group.name)
-    else:
-        gitlab_group = mygitlab.mygroup_all.search(name=ldap_group.name)
-        if gitlab_group is not None:
-            absense_items = gitlab_group.check(red_list=ldap_group.member,
-                                               attr=check_attr
-                                               )[0]
-    return create, gitlab_group, absense_items
-
-
-def create_user_in_gitlab_by_ldap(dn: str, myldap: myLDAP, mygitlab: MyGitlab, ldap_provider='ldapmain') -> int:
-    user_attr = myldap.user_info(dn=dn)
-    user = ldap_user_to_gitlab(ldap_user_attr=user_attr,
-                               ldap_provider=ldap_provider,
-                               dn=dn
-                               )
-    return mygitlab.user_create(info=user)
-
-
-def modify_group_user_into_gitlab_from_ldap(myldap: myLDAP,
-                                            mygitlab: MyGitlab,
-                                            ldap_group: SimpleGroup,
-                                            create_user: bool = False,
-                                            new_group_visibility='private',
-                                            ldap_provider='ldapmain'
-                                            ) -> None:
-    results = check_group_member_in_gitlab(mygitlab=mygitlab,
-                                           ldap_group=ldap_group,
-                                           new_group_visibility=new_group_visibility
-                                           )
-    create, gitlab_group, absense_items = results
-    for item in absense_items:
-        user = mygitlab.myuser_all.search_by_ext_uid(extern_uid=item)
-        if user is None and create_user:
-            try:
-                user_id = create_user_in_gitlab_by_ldap(dn=item,
-                                                        myldap=myldap,
-                                                        mygitlab=mygitlab,
-                                                        ldap_provider=ldap_provider
-                                                        )
-            except:
-                continue
-        else:
-            user_id = user.id
-        if user_id is not None:
-            mygitlab.group_add_member(group_info=gitlab_group, user_info=user, access_level=DEVELOPER_ACCESS)
-
-
-def sync_exec(myldap: myLDAP,
-              mygitlab: MyGitlab,
-              ldap_group_list: SimpleGroupList,
-              create_user: bool = False,
-              new_group_visibility: str = 'private',
-              ldap_provider: str = 'ldapmain'
-              ) -> None:
-    for ldap_group in ldap_group_list:
-        try:
-            modify_group_user_into_gitlab_from_ldap(myldap=myldap,
-                                                    mygitlab=mygitlab,
-                                                    ldap_group=ldap_group,
-                                                    create_user=create_user,
-                                                    new_group_visibility=new_group_visibility,
-                                                    ldap_provider=ldap_provider
-                                                    )
-        except:
-            pass
-
-
-def sync():
-    url = 'https://gitlab.iamchentao.com'
-    access = 'glpat-AMZWLMfK2vGimyhzssz3'
-
-    host = "ldap.iamchentao.com"
-    admin = 'cn=admin,dc=lab,dc=com'
-    passwd = '0chen0TAO0@'
-    base_user = 'ou=People,ou=ETH,dc=lab,dc=com'
-    base_group = 'ou=ExperimentalQuantumEngineering,ou=PHY,ou=Depart,ou=ETH,dc=lab,dc=com'
-    classname = ["groupOfUniqueNames", "posixGroup"]
-    # pattern="M*"
-    pattern = None
-
-    mygitlab = MyGitlab(url=url, access_token=access, ssl_verify=True)
-    mygitlab.connect()
-
-    condition = GroupSearchCon(base=base_group, name_like=pattern, classname=classname)
-    user_con = UserSearchCon(base=base_user)
-    ldap = LDAP(host=host, admin=admin, password=passwd)
-    myldap = myLDAP(ldap=ldap)
-    gitlab_gm = mygitlab.get_group_member_all()
-    gitlab_g = mygitlab.get_groups_all_names()
-    ldap_gm = myldap.get_users(group_Con=condition, user_Con=user_con)
-
-
 @dataclass
 class MyConfig:
 
@@ -144,6 +17,7 @@ class MyConfig:
     def asdict(self) -> dict[str, Any]:
         return deepcopy(asdict(self))
 
+
 @dataclass
 class LDAP_Config(MyConfig):
     host: str = ""
@@ -154,19 +28,33 @@ class LDAP_Config(MyConfig):
     group_class: list[str] = field(default_factory=lambda: ["groupOfUniqueNames", "posixGroup"])
     group_like = '*'
 
+
 @dataclass
 class Gitlab_Config(MyConfig):
     url: str = ''
     access: str = ''
+    check_attr: str = 'extern_uid'
+    ssl_verify: bool = True
     create_user: bool = False
     new_group_visibility: str = 'private'
     ldap_provider: str = 'ldapmain'
 
 
 @dataclass
-class Sync:
+class Sync_Config:
     gitlab: Gitlab_Config = field(default_factory=Gitlab_Config)
-    LDAP: Gitlab_Config = field(default_factory=LDAP_Config)
+    LDAP: LDAP_Config = field(default_factory=LDAP_Config)
+
+    @property
+    def user_con(self) -> UserSearchCon:
+        return UserSearchCon(base=self.LDAP.base_user)
+
+    @property
+    def group_con(self) -> UserSearchCon:
+        condition = GroupSearchCon(base=self.LDAP.base_group,
+                                   name_like=self.LDAP.group_like,
+                                   classname=self.LDAP.group_class)
+        return condition
 
     def read_from_json(self, filename='./config.json') -> None:
         with open(filename) as f:
@@ -178,8 +66,107 @@ class Sync:
         with open(filename, 'w') as f:
             json.dump(asdict(self), f, indent=4)
 
+    def config_to(self) -> tuple[myLDAP, MyGitlab]:
+        ldap_config = self.LDAP
+        gitlab_config = self.gitlab
+        mygitlab = MyGitlab(url=gitlab_config.url, access_token=gitlab_config.access, ssl_verify=gitlab_config.ssl_verify)
+        mygitlab.connect()
+
+        ldap = LDAP(host=ldap_config.host,
+                    admin=ldap_config.admin,
+                    password=ldap_config.password)
+        myldap = myLDAP(ldap=ldap)
+        return myldap, mygitlab
+
+
+class Sync:
+    def __init__(self, config: str) -> None:
+        self.__config: Sync_Config = Sync_Config()
+        self.__myldap: myLDAP = None
+        self.__mygitlab: MyGitlab = None
+        try:
+            self.init(config=config)
+        except:
+            pass
+
+    @property
+    def config(self) -> Sync_Config:
+        return self.__config
+
+    @property
+    def myldap(self) -> myLDAP:
+        return self.__myldap
+
+    @property
+    def mygitlab(self) -> MyGitlab:
+        return self.__mygitlab
+
+    def init(self, config: str) -> None:
+        self.__config.read_from_json(config)
+        self.__myldap, self.__mygitlab = self.__config.config_to()
+
+    def ldap_user_to_gitlab(self, ldap_user_attr: dict[int, Any], dn: str) -> MyUser:
+        user = MyUser(id=-1,
+                      username=ldap_user_attr['uid'],
+                      name=ldap_user_attr['cn'],
+                      email=ldap_user_attr['mail']
+                      )
+        user.ext_ID.uid = dn
+        user.ext_ID.provider = self.config.gitlab.ldap_provider
+        return user
+
+    def create_group_in_gitlab_by_ldap(self, ldap_group: SimpleGroup) -> int:
+        group_info = GitlabGroup(name=ldap_group.name, visibility=self.config.gitlab.new_group_visibility)
+        if ldap_group.description is not None and ldap_group.description == '':
+            group_info.description = ldap_group.description
+        group_id = self.mygitlab.group_create(group_info)
+        return group_id
+
+    def check_group_member_in_gitlab(self, ldap_group: SimpleGroup) -> tuple[bool, MyGroup, list[str]]:
+        absense_items = ldap_group.members
+        gitlab_group_names = self.mygitlab.mygroup_all.names
+        create = ldap_group.name not in gitlab_group_names
+        if create:
+            group_id = self.create_group_in_gitlab_by_ldap(self, ldap_group=ldap_group)
+            gitlab_group = MyGroup(id=group_id, name=ldap_group.name)
+        else:
+            gitlab_group = self.mygitlab.mygroup_all.search(name=ldap_group.name)
+            if gitlab_group is not None:
+                absense_items = gitlab_group.check(red_list=ldap_group.member, attr=self.config.gitlab.check_attr)[0]
+        return create, gitlab_group, absense_items
+
+    def create_user_in_gitlab_by_ldap(self, dn: str) -> int:
+        user_attr = self.myldap.user_info(dn=dn)
+        user = self.ldap_user_to_gitlab(ldap_user_attr=user_attr, dn=dn)
+        return self.mygitlab.user_create(info=user)
+
+    def modify_group_user_into_gitlab_from_ldap(self, ldap_group: SimpleGroup) -> None:
+        results = self.check_group_member_in_gitlab(ldap_group=ldap_group)
+        create, gitlab_group, absense_items = results
+        for item in absense_items:
+            user = self.mygitlab.myuser_all.search_by_ext_uid(extern_uid=item)
+            if user is None and self.config.gitlab.create_user:
+                try:
+                    user_id = self.create_user_in_gitlab_by_ldap(dn=item)
+                except:
+                    continue
+            else:
+                user_id = user.id
+            if user_id is not None:
+                self.mygitlab.group_add_member(group_info=gitlab_group, user_info=user, access_level=DEVELOPER_ACCESS)
+
+    def sync(self) -> None:
+        ldap_group_list = self.myldap.get_users(group_Con=self.config.group_con, user_Con=self.config.user_con)
+        for ldap_group in ldap_group_list:
+            try:
+                self.modify_group_user_into_gitlab_from_ldap(ldap_group=ldap_group)
+            except:
+                pass
+
 
 if __name__ == '__main__':
-    a = Sync()
-    # print(a)
-    a.weite_to_json()
+    # a = Sync_Config()
+    # a.weite_to_json()
+    sync=Sync(config='./config.json')
+    sync.sync()
+    
